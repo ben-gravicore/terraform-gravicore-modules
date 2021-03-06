7c9cbd298d65cef1c69481f8528d2f3cc811149f
Benjamin Chung <benjamin.chung@celink.com>
2021-03-05 17:45:12 -0800

    add variable for default aws regions

--- aws/security-defaults/module.tf
+++ aws/security-defaults/module.tf
@@ -18,10 +18,10 @@ variable terraform_module {
   description = "The owner and name of the Terraform module"
 }
 
-variable "aws_regions" {
-  type        = list(string)
-  description = "List of the AWS regionsn to apply security groups into"
-  default     = ["ap-northeast-1", "ap-northeast-2", "ap-south-1", "ap-southeast-1", "ap-souteast-2", "ca-central-1", "eu-central-1", "eu-north-1", "eu-west-1", "eu-west-2", "eu-west-3", "sa-east-1", "us-east-2", "us-west-1", "us-west-2"]
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "The AWS region to deploy module into"
 }
 
 variable "create" {
--- aws/security-defaults/security-groups.tf
+++ aws/security-defaults/security-groups.tf
@@ -1,3 +1,9 @@
+variable "default_security_deny_all_traffic_on_aws_regions" {
+  type        = list(string)
+  description = "List of the AWS regionsn to apply security groups into"
+  default     = ["ap-northeast-1", "ap-northeast-2", "ap-south-1", "ap-southeast-1", "ap-souteast-2", "ca-central-1", "eu-central-1", "eu-north-1", "eu-west-1", "eu-west-2", "eu-west-3", "sa-east-1", "us-east-2", "us-west-1", "us-west-2"]
+}
+
 variable "default_security_deny_all_traffic" {
   type        = bool
   description = "Update the rules for the default security groups to deny all traffic by default?"
@@ -8,5 +14,21 @@ data "aws_security_groups" "defaults" {
   filter {
     name   = "group-name"
     values = ["*nodes*"]
+  }
+}
+
+resource "aws_security_group" "deny_all_traffic" {
+  name        = "deny_all_traffic"
+  description = "Deny all traffic on default security groups"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+  }
+
+  egress {
+  }
+
+  tags = {
+    Name = local.tags
   }
 }
