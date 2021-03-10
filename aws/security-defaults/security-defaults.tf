53ea3e1f91d33cc0fc792e748b03611012d847fe
Benjamin Chung <benjamin.chung@celink.com>
2021-03-10 08:26:29 -0800

    test using aws_default_security_group resource instead

--- aws/security-defaults/security-defaults.tf
+++ aws/security-defaults/security-defaults.tf
@@ -4,28 +4,23 @@ variable "default_security_deny_all_traffic_on_aws_regions" {
   default     = ["ap-northeast-1", "ap-northeast-2", "ap-south-1", "ap-southeast-1", "ap-souteast-2", "ca-central-1", "eu-central-1", "eu-north-1", "eu-west-1", "eu-west-2", "eu-west-3", "sa-east-1", "us-east-2", "us-west-1", "us-west-2"]
 }
 
-variable "default_aws_security_groups" {
+variable "default_aws_security_group_vpc_ids" {
   type = list(object({
-    security_group_id     = string
+    vpc_id                = string
     region                = string
   }))
 
-  description = "List of security groups with region"
+  description = "List of vpc_ids from default security groups with region"
   default     = []
 }
 
-resource "aws_security_group" "deny_all_traffic" {
+resource "aws_default_security_group" "deny_all_traffic" {
   # vpc_id      = data.aws_vpc.default.id
-  ingress {
-  }
 
-  dynamic "security_groups" {
-    for_each = var.default_aws_security_groups
+  dynamic "vpc_id" {
+    for_each = var.vpc_ids
     content {
-      security_groups   = default_aws_security_groups.value.security_group_id
+      vpc_id   = vpc_ids.value.vpc_id
     }
-  }
-
-  egress {
   }
 }
