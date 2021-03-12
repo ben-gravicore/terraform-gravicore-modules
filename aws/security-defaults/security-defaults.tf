9324e5659c385c9ac6548806182ed67762e7f2a7
Benjamin Chung <benjamin.chung@celink.com>
2021-03-11 18:10:27 -0800

    set vpc_id on security group

--- aws/security-defaults/security-defaults.tf
+++ aws/security-defaults/security-defaults.tf
@@ -1,25 +1,9 @@
-variable "default_security_deny_all_traffic_on_aws_regions" {
-  type        = list(string)
-  description = "List of the AWS regionsn to apply security groups into"
-  default     = ["ap-northeast-1", "ap-northeast-2", "ap-south-1", "ap-southeast-1", "ap-souteast-2", "ca-central-1", "eu-central-1", "eu-north-1", "eu-west-1", "eu-west-2", "eu-west-3", "sa-east-1", "us-east-2", "us-west-1", "us-west-2"]
-}
-
-variable "default_aws_security_group_vpc_ids" {
-  type = list(object({
-    vpc_id                = string
-  }))
-
-  description = "List of vpc_ids from default security groups with region"
-  default     = []
+variable "default_aws_security_group_vpc_id" {
+  type        = string
+  default     = ""
+  description = "vpc_id of default security group"
 }
 
 resource "aws_default_security_group" "default" {
-  # vpc_id      = data.aws_vpc.default.id
-
-#   dynamic "vpc_id" {
-#      for_each = var.default_aws_security_group_vpc_ids
-#      content {
-#        vpc_id   = default_aws_security_group_vpc_ids.value.vpc_id
-#      }
-#  }
+  vpc_id      = var.default_aws_security_group_vpc_id.value
 }
